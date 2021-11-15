4a3e69c23893cbd4bbba84134f9b1d5a1fef29d5
TheCad <mbeijen1@gmail.com>
2021-11-15 20:11:49 +0100

    Add template code for all days

---
+++ day-05/day-05.php
@@ -0,0 +1,32 @@
+<?php
+require(__DIR__ . '/../vendor/autoload.php');
+
+class Day05 {
+    public array $data;
+
+    public function __construct() {
+        $this->data = array();
+        $this->loadData();
+    }
+
+    public function loadData() {
+        $handle = fopen(__DIR__ . '/input', 'r');
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                array_push($this->data, trim($line));
+            }
+
+            fclose($handle);
+        } else {
+            echo "Can't find file";
+        }
+
+        $this->solve();
+    }
+
+    public function solve() {
+        dump($this->data);
+    }
+}
+
+new Day05();
