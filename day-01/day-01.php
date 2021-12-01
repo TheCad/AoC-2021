b000ad0a904b35bd29ba69ae0da634df8adadda8
Marco Beijen <mbeijen1@gmail.com>
2021-12-01 09:53:07 +0100

    Added day 1

--- day-01/day-01.php
+++ day-01/day-01.php
@@ -25,7 +25,35 @@ class Day01 {
     }
 
     public function solve() {
-        dump($this->data);
+//        echo $this->solveChallenge1();
+        echo $this->solveChallenge2();
+    }
+
+    public function solveChallenge1(): int {
+        $increments = 0;
+        for ($i = 0; $i < count($this->data); $i++) {
+            if ($i == count($this->data) - 1)
+                break;
+            if ($this->data[$i+1] > $this->data[$i])
+                $increments++;
+        }
+        return $increments;
+    }
+
+    public function solveChallenge2(): int {
+        $increments = 0;
+        for ($i = 0; $i < count($this->data); $i++) {
+            if ($i >= count($this->data) - 3){
+                break;
+            }
+            $sum1 = $this->data[$i] + $this->data[$i+1] + $this->data[$i+2];
+            $sum2 = $this->data[$i+1] + $this->data[$i+2] + $this->data[$i+3];
+
+            if ($sum2 > $sum1)
+                $increments++;
+        }
+
+        return $increments;
     }
 }
 
