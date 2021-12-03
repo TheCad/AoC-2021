7b7373bbffc418de28b705856e10920b6f79e9c5
TheCad <mbeijen1@gmail.com>
2021-12-03 09:12:04 +0100

    Add part 1 of day 3
    Part 2 is too much reading in the morning
    I need coffee for this

--- day-03/day-03.php
+++ day-03/day-03.php
@@ -25,7 +25,45 @@ class Day03 {
     }
 
     public function solve() {
-        dump($this->data);
+        dump($this->solveChallenge1());
+        dump($this->solveChallenge2());
+    }
+
+    private function solveChallenge1() {
+        $strlen = strlen($this->data[0]);
+        $amount = array_fill(0, $strlen, array_fill(0,2,null));
+        $gamma = "";
+        $epsilon ="";
+
+        foreach ($this->data as $line) {
+            for($i = 0; $i < $strlen; $i++) {
+                switch ($line[$i]) {
+                    case 0:
+                        $amount[$i][0]++;
+                        break;
+                    case 1:
+                        $amount[$i][1]++;
+                        break;
+                }
+            }
+        }
+
+        foreach ($amount as $line) {
+            if ($line[0] > $line[1]) {
+                $gamma .= "0";
+                $epsilon .= "1";
+            }
+            else {
+                $gamma .= "1";
+                $epsilon .= "0";
+            }
+        }
+
+        return (bindec($gamma) * bindec($epsilon));
+    }
+
+    private function solveChallenge2() {
+
     }
 }
 
