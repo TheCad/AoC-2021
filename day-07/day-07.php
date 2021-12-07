c1589668e70933b8f82110e3661cb17191beb196
TheCad <mbeijen1@gmail.com>
2021-12-07 12:36:54 +0100

    wtf is day 7

--- day-07/day-07.php
+++ day-07/day-07.php
@@ -15,7 +15,7 @@ class Day07 {
         $handle = fopen(__DIR__ . $file, 'r');
         if ($handle) {
             while (($line = fgets($handle)) !== false) {
-                array_push($this->data, trim($line));
+                $this->data = explode(',', trim($line));
             }
 
             fclose($handle);
@@ -27,7 +27,55 @@ class Day07 {
     }
 
     public function solve() {
-        dump($this->data);
+        dump($this->solveChallenge1());
+        dump($this->solveChallenge2());
+    }
+
+    public function solveChallenge1() {
+        $amount = PHP_INT_MAX;
+        $list = array_count_values($this->data);
+        arsort($list);
+        foreach ($list as $item => $value) {
+            $fuel = $this->getAmountOfFuel($item);
+            $amount = (array_sum($fuel) < $amount) ? array_sum($fuel) : $amount;
+        }
+
+        return $amount;
+    }
+
+    public function solveChallenge2() {
+        $amount = PHP_INT_MAX;
+        $res = [];
+
+        foreach (range(0, max($this->data), 1) as $item => $value) {
+            $fuel = array_sum($this->getAmountOfFuelIncremental($item));
+            $res[] = $fuel;
+        }
+        sort($res);
+        return $res[0];
+    }
+
+    public function getAmountOfFuel($horpos) {
+        $temp = [];
+        foreach ($this->data as $item) {
+            $fuelcost = $horpos - $item;
+            if ($fuelcost < 0) $fuelcost = -$fuelcost;
+            $temp[] = $fuelcost;
+        }
+        return $temp;
+    }
+
+    public function getAmountOfFuelIncremental($horpos) {
+        $temp = [];
+
+        foreach ($this->data as $item) {
+            $fuelcost = $horpos - $item;
+            if ($fuelcost < 0) $fuelcost = -$fuelcost;
+            $temp[] = array_sum(range(1, $fuelcost, 1));
+        }
+        $res = array_sum($temp);
+
+        return $temp;
     }
 }
 
