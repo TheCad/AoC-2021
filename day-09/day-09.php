5cfc89d199e1bd5c52bde3c9760da65a50cd864f
Marco Beijen <mbeijen1@gmail.com>
2021-12-09 19:02:23 +0100

    Temporary commit

--- day-09/day-09.php
+++ day-09/day-09.php
@@ -2,11 +2,13 @@
 require(__DIR__ . '/../vendor/autoload.php');
 
 class Day09 {
-    public bool $example = false;
+    public bool $example = true;
     public array $data;
+    public array $list;
 
     public function __construct() {
         $this->data = array();
+        $this->list = array();
         $this->loadData();
     }
 
@@ -27,7 +29,41 @@ class Day09 {
     }
 
     public function solve() {
-        dump($this->data);
+        foreach ($this->data as $key => $line) {
+            $this->list[$key] = str_split($line);
+        }
+        dump($this->solveChallenge1());
+        dump($this->solveChallenge2());
+    }
+
+    private function solveChallenge1() {
+        $smallest = false;
+        for ($y = 0; $y < count($this->list); $y++) {
+            for ($x = 0; $x < count($this->list[$y]); $x++) {
+                $this->checkIsSmallest($x, $y);
+            }
+        }
+        dd($this->list);
+    }
+
+    private function solveChallenge2() {
+
+    }
+
+    private function checkIsSmallest(?int $x, ?int $y) {
+        $number = $this->list[$y][$x];
+        $up = array_key_exists($y - 1, $this->list[$x]) ? $this->list[$x][$y-1] : null;
+        $down = array_key_exists($y + 1, $this->list[$x]) ? $this->list[$x][$y+1] : null;
+        $right = array_key_exists($x+1, $this->list) ? $this->list[$x + 1][$y]: null;
+        $left = array_key_exists($x - 1, $this->list) ? $this->list[$x - 1][$y] : null;
+
+        if (($up !== null && $up > $number) &&
+            ($down !== null&& $down > $number) &&
+            ($left !== null && $left > $number) &&
+            ($right !== null && $right > $number)) {
+
+        }
+
     }
 }
 
